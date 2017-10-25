6bca6b1eaaef6278943d789a287e5082e8810cd3
Angular CLI <angular-cli@angular.io>
2017-10-25 08:28:28 -0300

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'h2-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'h2';
+}
